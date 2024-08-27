90d45f06cc1966f2b9c94fbd72eb6c02b6876ada
sail <liaoleon000513@gmail.com>
2024-08-27 21:18:24 +0800

    feat: add Login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import { useState } from 'react';
-import { Button } from '@/components/ui/button';
+import Login from './pages/Login';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <Button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="flex justify-center items-center h-screen">
+        <Login />
       </div>
     </>
   );
---
+++ src/pages/Login.tsx
@@ -0,0 +1,51 @@
+import {
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  FormField,
+} from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+
+import { useForm } from 'react-hook-form';
+import { Input } from '@/components/ui/input';
+
+const Login = () => {
+  const form = useForm();
+  return (
+    <Form {...form}>
+      <form className="flex flex-col gap-4 border border-gray-300 p-12 rounded-md">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="your password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button>Login</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default Login;
