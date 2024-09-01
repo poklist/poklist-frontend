a62a920849619a0f652b11574addf7f1803169a9
sail <liaoleon000513@gmail.com>
2024-09-01 21:28:51 +0800

    fix: unresolved merge confilcts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 
-import MainContainer from './components/ui/containers/MainContainer';
-
 function App() {
   return <RouterProvider router={router} />;
 }
