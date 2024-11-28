e9a8d9a6e44a15843e849b0bdb30d7bf30e9c361
D9niel <d9niel.code@gmail.com>
2024-11-29 04:28:11 +0800

    chore: remove unused variables

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,7 +1,5 @@
 import MobileContainer from '@/components/ui/containers/MobileContainer';
-import useIsMyPage from '@/hooks/useIsMyPage';
 import axios from '@/lib/axios';
-import useUserStore from '@/stores/useUserStore';
 import { User } from '@/types/User';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
@@ -14,13 +12,11 @@ interface UserPageProps {
 
 const UserPage: React.FC<UserPageProps> = () => {
   const { id } = useParams();
-  const { isLoggedIn } = useUserStore();
   const [viewUser, setViewUser] = useState<User>({
     id: 0,
     displayName: '',
     userCode: '',
   });
-  const isMyPage = useIsMyPage(id);
 
   const getUser = async () => {
     axios.get(`/users/${id}`).then((res: any) => {
