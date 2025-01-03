019c58d0e6a88a077e5012593c15d4e731d9a7df
jamespig <james7766646@gmail.com>
2025-01-03 16:09:38 +0800

    Revert "feat: install Netlify package, add Netlify build file,adjust vite config"
    This reverts commit 529c300d7e87470d4c2babd5858044f7542e42d3.

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,17 @@
-import { lingui } from '@lingui/vite-plugin';
-import react from '@vitejs/plugin-react';
-import path from 'node:path';
-import { defineConfig } from 'vite';
+import { lingui } from "@lingui/vite-plugin";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env': {},
+    "process.env": {}
   },
-  plugins: [react({ babel: { plugins: ['macros'] } }), lingui()],
+  plugins: [react({ babel: { plugins: ["macros"] } }), lingui()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
+      "@": path.resolve(__dirname, "src")
+    }
+  }
 });
