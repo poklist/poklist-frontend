2ea0bf065e77ca7c57be458cd4da53c2001c6a20
jamespig <james7766646@gmail.com>
2025-01-20 17:59:19 +0800

    feat: Add fake pictures, home introduction constants

---
+++ src/constants/Home/index.en.ts
@@ -0,0 +1,305 @@
+export const HERO_SECTION = {
+  joinInformation: {
+    title: 'Join Creator Account',
+    descriprion:
+      'For now, only content creators can apply for a “Creator Account.',
+    buttonText: 'Apply for Creator Account?',
+  },
+  accountOwner: {
+    title: 'Got a Creator Account',
+    buttonText: 'Sign in with Google',
+  },
+  nonCreatorQuestion: 'Can non-creators use Poklist?',
+};
+export const FEATURE_SECTION = {
+  title: 'List Your Ideas',
+  descriprion:
+    'Turn thoughts into lists, discover fresh finds, and follow inspiration.',
+};
+export const LIST_SECTION = {
+  lifeStyle: {
+    title: 'Lifestyle',
+    user: 'Stacy Chambers',
+    account: '＠thechambers',
+    userAvatar: '',
+    listCount: '24',
+    listTitle: 'Things I do during my day off',
+    lists: [
+      {
+        id: 1,
+        title: 'Things I do during my day off',
+        description: 'Açaí bowl from the local farmer’s market.',
+      },
+      {
+        id: 2,
+        title: 'Snack',
+        description: 'Home-brewed matcha latte with almond...',
+      },
+      {
+        id: 3,
+        title: 'Meals',
+        description: 'Spontaneous taco stop at a hidden gem f...',
+      },
+    ],
+  },
+  foodAndDrink: {
+    title: 'Food & Drink',
+    user: '小資一姐',
+    account: '＠costlady',
+    userAvatar: '',
+    listCount: '12',
+    listTitle: 'Skin care for summer chill girl',
+    lists: [
+      {
+        id: 1,
+        title: 'Lightweight Gel Moisturizer',
+        description: 'Hydrates without feeling heavy or sticky—...',
+        image: '',
+      },
+      {
+        id: 2,
+        title: 'Broad-Spectrum Sunscreen (SPF 50)',
+        description: 'Protects your skin from harmful UV rays w...',
+        image: '',
+      },
+      {
+        id: 3,
+        title: 'Refreshing Face Mist',
+        description: 'Keeps your skin cool and fresh throughout...',
+        image: '',
+      },
+    ],
+  },
+  culture: {
+    title: 'Culture',
+    user: 'Campus Tips',
+    account: '＠campustips',
+    userAvatar: '',
+    listCount: '19',
+    listTitle: 'Pack for Campus Fest',
+    lists: [
+      {
+        id: 1,
+        title: 'Essentials',
+        description: 'Sunscreen, water bottle, and comfy shoes.',
+        image: '',
+      },
+      {
+        id: 2,
+        title: 'Extras',
+        description: 'A light jacket for when it gets chilly.',
+        image: '',
+      },
+      {
+        id: 3,
+        title: 'Pro tip:',
+        description: 'Bring a blanket to claim your spot early.',
+        image: '',
+      },
+    ],
+  },
+  travel: {
+    title: 'Travel',
+    user: 'SF LIFE',
+    account: '＠sflifenews',
+    userAvatar: '',
+    listCount: '8',
+    listTitle: 'SF to wine country tips',
+    lists: [
+      {
+        id: 1,
+        title: 'Plan Your Route',
+        description: 'Stick to Napa or Sonoma for a manageabl...',
+        image: '',
+      },
+      {
+        id: 2,
+        title: 'Dress for Comfort',
+        description: 'Wear layers: Mornings are cool, afternoon...',
+        image: '',
+      },
+      {
+        id: 3,
+        title: 'Sip Smart and Stay Safe',
+        description: 'Limit yourself: Plan 2–3 wineries to enjoy t...',
+        image: '',
+      },
+    ],
+  },
+  entertainment: {
+    title: 'Entertainment',
+    user: 'Viv Wang',
+    account: '＠vivwang',
+    userAvatar: '',
+    listCount: '22',
+    listTitle: 'Outdoor movie nights in SF',
+    lists: [
+      {
+        id: 1,
+        title: 'Fort Mason Flix',
+        description: 'Legendary! Movies with a bay view!',
+        image: '',
+      },
+      {
+        id: 2,
+        title: 'Alameda Backyard Cinema',
+        description: 'Just went last night and it was as awesome...',
+        image: '',
+      },
+      {
+        id: 3,
+        title: 'Dolores Park Movie Nights',
+        description: 'Remember to bring your blanket and snac...',
+      },
+    ],
+  },
+  techAndDigital: {
+    title: 'Tech & Digital',
+    user: 'Nex Gen',
+    account: '＠nexgenlist',
+    userAvatar: '',
+    listCount: '6',
+    listTitle: 'How I curate my digital space',
+    lists: [
+      {
+        id: 1,
+        title: 'Social media',
+        description: 'Following people who inspire, not drain.',
+        image: '',
+      },
+      {
+        id: 2,
+        title: 'Desktop',
+        description: 'Clearing duplicates and random screensh...',
+        image: '',
+      },
+      {
+        id: 3,
+        title: 'Notes app',
+        description: 'Organized folders for every thought.',
+        image: '',
+      },
+    ],
+  },
+  personalGrowth: {
+    title: 'Personal Growth',
+    user: 'Corp Girlies ',
+    account: '＠corpgirlies',
+    userAvatar: '',
+    listCount: '24',
+    listTitle: 'Lessons from my morning walks',
+    lists: [
+      {
+        id: 1,
+        title: 'Morning',
+        description: 'Weirdly liberating but missed my alarm',
+        image: '',
+      },
+      {
+        id: 2,
+        title: 'Noon',
+        description: 'Felt more present during lunch with friends.',
+        image: '',
+      },
+      {
+        id: 3,
+        title: 'Evening',
+        description: 'Read a book before bed, no distractions',
+        image: '',
+      },
+    ],
+  },
+  healthAndFitness: {
+    title: 'Health & Fitness',
+    user: 'Wellness Hub',
+    account: '＠towellness',
+    userAvatar: '',
+    listCount: '41',
+    listTitle: 'Quick dorm room workouts',
+    lists: [
+      {
+        id: 1,
+        title: '5 push-ups',
+        description: 'Not much, but it’s a start.',
+        image: '',
+      },
+      {
+        id: 2,
+        title: 'Wall sits',
+        description: 'Burned like fire, but worth it.',
+        image: '',
+      },
+      {
+        id: 3,
+        title: 'Desk stretches',
+        description: 'Especially after long study sessions.',
+        image: '',
+      },
+    ],
+  },
+  other: {
+    title: 'Other',
+    user: 'Jenny PHD',
+    account: '＠Jennyphd',
+    userAvatar: '',
+    listCount: '13',
+    listTitle: 'My favorite study spot hunt',
+    lists: [
+      {
+        id: 1,
+        title: 'Library basement',
+        description: 'Quiet but kinda creepy.',
+        image: '',
+      },
+      {
+        id: 2,
+        title: 'Cafe near campus',
+        description: 'Lattes + free Wi-Fi.',
+        image: '',
+      },
+      {
+        id: 3,
+        title: 'Student union',
+        description: 'Snacks within reach at all times.',
+        image: '',
+      },
+    ],
+  },
+};
+export const TUTORIAL_SECTION = [
+  { title: 'About Poklist', url: '' },
+  {
+    title: 'Tutorials',
+    url: '',
+  },
+  {
+    title: 'FAQ',
+    url: '',
+  },
+  {
+    title: 'Join the Team',
+    url: '',
+  },
+];
+export const FOOTER_SECTION = [
+  {
+    title: 'Press kit',
+    url: '',
+  },
+  {
+    title: 'Contact Us',
+    url: '',
+  },
+  {
+    title: 'Terms',
+    url: '',
+  },
+  {
+    title: 'Privacy',
+    url: '',
+  },
+  {
+    title: 'Collection Notice',
+    url: '',
+  },
+];
---
+++ src/constants/Home/index.zh.ts
@@ -0,0 +1,307 @@
+export const HERO_SECTION = {
+  joinInformation: {
+    title: '搶先加入Poklist',
+    descriprion: '目前僅限內容創作者申請「創作者帳號」。',
+    buttonText: '如何申請創作者帳號？',
+  },
+  accountOwner: {
+    title: '我擁有創作者帳號!',
+    buttonText: 'Google 登入',
+  },
+  nonCreatorQuestion: '非內容創作者也能使用 Poklist 嗎？',
+};
+export const FEATURE_SECTION = {
+  title: '創作我的靈感名單',
+  descriprion: '獵奇有趣的事物，創作新奇的名單，追蹤超有料的好靈感。',
+};
+export const LIST_SECTION = {
+  lifeStyle: {
+    title: '生活風格',
+    user: '小資一姐',
+    account: '＠costlady',
+    userAvatar: '',
+    listCount: '12',
+    listTitle: '8月小資日常必備清單',
+    lists: [
+      {
+        id: 1,
+        title: '折疊購物袋',
+        description: '環保又方便，隨身攜帶超實用。 尤其是在...',
+      },
+      {
+        id: 2,
+        title: '定時咖啡機',
+        description: '早晨神救援，一醒來就有熱咖啡，我覺得...',
+      },
+      {
+        id: 3,
+        title: '迷你空氣清淨機',
+        description: '最近過敏鼻子整組壞掉，好險有書桌上的...',
+      },
+    ],
+  },
+  foodAndDrink: {
+    title: '美食',
+    user: '吃客先生',
+    account: '＠haday1',
+    userAvatar: '',
+    listCount: '8',
+    listTitle: '老饕牛肉麵-食記篇',
+    lists: [
+      {
+        id: 1,
+        title: '2月11日 永康牛肉麵',
+        description: '肉很嫩就贏我心！',
+        image: '',
+      },
+      {
+        id: 2,
+        title: '4月2日 廖家牛肉麵',
+        description: '老闆通常脾氣不太好，但是吃完之後心情...',
+        image: '',
+      },
+      {
+        id: 3,
+        title: '5月9日 泉新牛肉麵',
+        description: '承三代，老味道都沒變過！',
+        image: '',
+      },
+    ],
+  },
+  culture: {
+    title: '文化',
+    user: 'Stacy的生活',
+    account: '＠stacyworld',
+    userAvatar: '',
+    listCount: '3',
+    listTitle: '今年說好要去的音樂祭',
+    lists: [
+      {
+        id: 1,
+        title: '爵士音樂節',
+        description: '看了LaLaLand之後，突然被感染爵士魂...',
+        image: '',
+      },
+      {
+        id: 2,
+        title: '春天吶喊',
+        description: '骨灰級的音樂祭，感覺至少要去一次才對...',
+        image: '',
+      },
+      {
+        id: 3,
+        title: '太平洋音樂季',
+        description: '去年看到朋友去，整個超羨慕！',
+        image: '',
+      },
+    ],
+  },
+  travel: {
+    title: '旅遊',
+    user: '無聊就輸了',
+    account: '＠worldman',
+    userAvatar: '',
+    listCount: '25',
+    listTitle: '這週短途旅行的五個瞬間',
+    lists: [
+      {
+        id: 1,
+        title: '小鎮咖啡店',
+        description: '點了一杯招牌拿鐵，窗外的花園讓我差點...',
+        image: '',
+      },
+      {
+        id: 2,
+        title: '公路的風景',
+        description: '去嘉義的路上經過一片金黃色的稻田，覺得...',
+        image: '',
+      },
+      {
+        id: 3,
+        title: '海邊的晚霞',
+        description: '以前覺得海邊一定要夏天去，現在覺得晚...',
+        image: '',
+      },
+    ],
+  },
+  entertainment: {
+    title: '娛樂',
+    user: '小豬在睡覺',
+    account: '＠sleepybigs',
+    userAvatar: '',
+    listCount: '21',
+    listTitle: '那些讓我熬夜的台劇',
+    lists: [
+      {
+        id: 1,
+        title: '《想見你》',
+        description: '我永遠對那種愛情與時空交錯的劇情無法...',
+        image: '',
+      },
+      {
+        id: 2,
+        title: '《華燈初上》',
+        description: '之前大家都在看，現在才燒到我！',
+        image: '',
+      },
+      {
+        id: 3,
+        title: '《我們與惡的距離》',
+        description: '每次社會事件發生時，我都回想這部片，...',
+      },
+    ],
+  },
+  techAndDigital: {
+    title: '數位科技',
+    user: '讀讀科技',
+    account: '＠itech',
+    userAvatar: '',
+    listCount: '10',
+    listTitle: '懶人提升效率的科技小物',
+    lists: [
+      {
+        id: 1,
+        title: 'Notion',
+        description: '整合筆記與項目管理，效率爆表。一定要...',
+        image: '',
+      },
+      {
+        id: 2,
+        title: 'Google Drive',
+        description: '最近我連健身運動都用GD! 有興趣的可以...',
+        image: '',
+      },
+      {
+        id: 3,
+        title: 'ChatGPT',
+        description: 'AI真的省去我50%的工作時間，而且效率...',
+        image: '',
+      },
+    ],
+  },
+  personalGrowth: {
+    title: '個人成長',
+    user: '祥翔',
+    account: '＠viewshan',
+    userAvatar: '',
+    listCount: '6',
+    listTitle: '30歲改變三觀的好書（續更）',
+    lists: [
+      {
+        id: 1,
+        title: '《原子習慣》',
+        description: '從小改變累積成大成功！',
+        image: '',
+      },
+      {
+        id: 2,
+        title: '《被討厭的勇氣》',
+        description: '探索自我價值，擁抱真實人生。',
+        image: '',
+      },
+      {
+        id: 3,
+        title: '《心流》',
+        description: '進入心流，找到工作與生活的平衡。',
+        image: '',
+      },
+    ],
+  },
+  healthAndFitness: {
+    title: '健康與健身',
+    user: '匹克匹克',
+    account: '＠picapica',
+    userAvatar: '',
+    listCount: '17',
+    listTitle: '健身信徒必吃的食物',
+    lists: [
+      {
+        id: 1,
+        title: '全麥吐司',
+        description: '高纖低卡就對了！吃久了也習慣了！',
+        image: '',
+      },
+      {
+        id: 2,
+        title: '地瓜',
+        description: '最近用它取代甜點，似乎效果還不錯！',
+        image: '',
+      },
+      {
+        id: 3,
+        title: '堅果',
+        description: '隨手一定要有包堅果！',
+        image: '',
+      },
+    ],
+  },
+  other: {
+    title: '其他',
+    user: '想法王',
+    account: '＠johnthinker',
+    userAvatar: '',
+    listCount: '19',
+    listTitle: '約會靈感盡失的大推薦！',
+    lists: [
+      {
+        id: 1,
+        title: '手作陶藝課',
+        description: '做越醜越好看！這課程比我想像中的好玩...',
+        image: '',
+      },
+      {
+        id: 2,
+        title: '電影馬拉松',
+        description: '我覺得人生沒有電影馬拉松一下真的很可...',
+        image: '',
+      },
+      {
+        id: 3,
+        title: '跳蚤市場探險',
+        description: '週末超多跳蚤市場的，逛完去喝杯咖啡就...',
+        image: '',
+      },
+    ],
+  },
+};
+export const TUTORIAL_SECTION = [
+  {
+    title: '關於Poklist',
+    url: '',
+  },
+  {
+    title: '快速教學',
+    url: '',
+  },
+  {
+    title: '問與答',
+    url: '',
+  },
+  {
+    title: '加入團隊',
+    url: '',
+  },
+];
+
+export const FOOTER_SECTION = [
+  {
+    title: '媒體報導',
+    url: '',
+  },
+  {
+    title: '聯繫我們',
+    url: '',
+  },
+  {
+    title: '使用者條款',
+    url: '',
+  },
+  {
+    title: '隱私權保護政策',
+    url: '',
+  },
+  {
+    title: '額外公告',
+    url: '',
+  },
+];
