9075bd9f5b016c5b65349f379b645090a35bd2a6
jamespig <james7766646@gmail.com>
2025-01-21 14:06:13 +0800

    feat: Update home constants

---
+++ src/constants/Home/images.ts
@@ -0,0 +1,60 @@
+export const IMAGES = {
+  avatar: {
+    lifeStyle: '/src/assets/images/home/avatar/weeklythrifts.png',
+    foodAndDrink: '/src/assets/images/home/avatar/thechambers.png',
+    travel: '/src/assets/images/home/avatar/lifehacker.png',
+    culture: '/src/assets/images/home/avatar/campustips.png',
+    entertainment: '/src/assets/images/home/avatar/vivwang.png',
+    personalGrowth: '/src/assets/images/home/avatar/corpgirlies.png',
+    techAndDigital: '/src/assets/images/home/avatar/itech.png',
+    healthAndFitness: '/src/assets/images/home/avatar/picapica.png',
+    other: '/src/assets/images/home/avatar/jennyphd.png',
+  },
+  list: {
+    lifeStyle: {
+      list1: '/src/assets/images/home/lists/lifeStyle/shopping-bag.png',
+      list2: '/src/assets/images/home/lists/lifeStyle/coffee-machine.png',
+      list3: '/src/assets/images/home/lists/lifeStyle/air-purifier.png',
+    },
+    foodAndDrink: {
+      list1: '/src/assets/images/home/lists/foodAndDrink/breakfast.png',
+      list2: '/src/assets/images/home/lists/foodAndDrink/snack.png',
+      list3: '/src/assets/images/home/lists/foodAndDrink/meals.png',
+    },
+    culture: {
+      list1: '/src/assets/images/home/lists/culture/essentials.png',
+      list2: '/src/assets/images/home/lists/culture//extras.png',
+      list3: '/src/assets/images/home/lists/culture/pro-tips.png',
+    },
+    travel: {
+      list1: '/src/assets/images/home/lists/travel/total-budget.png',
+      list2: '/src/assets/images/home/lists/travel/hunt-deals.png',
+      list3: '/src/assets/images/home/lists/travel/transportation-hacks.png',
+    },
+    entertainment: {
+      list1: '/src/assets/images/home/lists/entertainment/Moana.png',
+      list2: '/src/assets/images/home/lists/entertainment/Tenet.png',
+      list3: '/src/assets/images/home/lists/entertainment/Dumb-Dumber.png',
+    },
+    techAndDigital: {
+      list1: '/src/assets/images/home/lists/techAndDigital/notion.png',
+      list2: '/src/assets/images/home/lists/techAndDigital/google-drive.png',
+      list3: '/src/assets/images/home/lists/techAndDigital/chat-gpt.png',
+    },
+    personalGrowth: {
+      list1: '/src/assets/images/home/lists/personalGrowth/start-small.png',
+      list2: '/src/assets/images/home/lists/personalGrowth/build-system.png',
+      list3: '/src/assets/images/home/lists/personalGrowth/embrace-new.png',
+    },
+    healthAndFitness: {
+      list1: '/src/assets/images/home/lists/healthAndFitness/ingredients.png',
+      list2: '/src/assets/images/home/lists/healthAndFitness/blend.png',
+      list3: '/src/assets/images/home/lists/healthAndFitness/add-extra.png',
+    },
+    other: {
+      list1: '/src/assets/images/home/lists/other/fish-pic.png',
+      list2: '/src/assets/images/home/lists/other/no-bio.png',
+      list3: '/src/assets/images/home/lists/other/dirty-room.png',
+    },
+  },
+};
