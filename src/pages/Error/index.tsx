96a67a8a58501ac6c57c3373a4eca30d161b9872
jamespig <james7766646@gmail.com>
2025-01-31 21:42:18 +0800

    feat: add i18n in Error Page

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -2,26 +2,30 @@ import mascotError from '@/assets/images/mascot/mascot-error.svg';
 import { Header } from '@/components/Header';
 import MobileContainer from '@/components/ui/containers/MobileContainer';
 import { TileBackground } from '../User/TileBackground';
+import { Trans } from '@lingui/macro';
+import { LanguageProvider } from '@/lib/languageProvider';
 
 export default function ErrorPage() {
   return (
-    <MobileContainer>
-      <TileBackground />
+    <LanguageProvider>
+      <MobileContainer>
+        <TileBackground />
 
-      <div className="relative z-10 flex min-h-screen flex-col">
-        {/* Header */}
-        <Header />
+        <div className="relative z-10 flex min-h-screen flex-col">
+          {/* Header */}
+          <Header />
 
-        {/* 內容 */}
-        <div className="flex flex-1 flex-col items-center justify-start pt-28">
-          <div className="flex flex-col items-center gap-6">
-            <img src={mascotError} alt="Error Mascot" className="h-[125px]" />
-            <h2 className="text-center text-[17px] font-bold text-black-text-01">
-              哎呀！網頁又在耍脾氣啦！
-            </h2>
+          {/* 內容 */}
+          <div className="flex flex-1 flex-col items-center justify-start pt-28">
+            <div className="flex flex-col items-center gap-6">
+              <img src={mascotError} alt="Error Mascot" className="h-[125px]" />
+              <h2 className="text-center text-[17px] font-bold text-black-text-01">
+                <Trans>哎呀！網頁又在耍脾氣啦！</Trans>
+              </h2>
+            </div>
           </div>
         </div>
-      </div>
-    </MobileContainer>
+      </MobileContainer>
+    </LanguageProvider>
   );
 }
