48f8e4cdb18b011a0f20bec912a783c94470209d
jamespig <james7766646@gmail.com>
2025-02-13 09:32:01 +0800

    fix: delete unnecessary code

--- src/types/response.ts
+++ src/types/response.ts
@@ -6,12 +6,3 @@ export interface IResponse<T> {
   limit?: number;
   totalElements?: number;
 }
-
-export interface IResponse<T> {
-  code?: number;
-  message?: string;
-  content?: T;
-  offset?: number;
-  limit?: number;
-  totalElements?: number;
-}
