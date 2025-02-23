9f5c999281d8d16be1e47eab7867a39e7e5928da
jamespig <james7766646@gmail.com>
2025-02-23 22:29:07 +0800

    fix: change image import logic

--- src/constants/Home/images.ts
+++ src/constants/Home/images.ts
@@ -1,69 +1,126 @@
+// avatar
+import lifeStyleAvatar from '@/assets/images/home/avatar/life-style.png';
+import foodAndDrinkAvatar from '@/assets/images/home/avatar/food-drink.png';
+import travelAvatar from '@/assets/images/home/avatar/travel.png';
+import cultureAvatar from '@/assets/images/home/avatar/culture.png';
+import entertainmentAvatar from '@/assets/images/home/avatar/entertainment.png';
+import personalGrowthAvatar from '@/assets/images/home/avatar/personal-growth.png';
+import techAndDigitalAvatar from '@/assets/images/home/avatar/tech-digital.png';
+import healthAndFitnessAvatar from '@/assets/images/home/avatar/health-fitness.png';
+import otherAvatar from '@/assets/images/home/avatar/other.png';
+
+// list
+import lifeStyleList1 from '@/assets/images/home/lists/lifeStyle/1.png';
+import lifeStyleList2 from '@/assets/images/home/lists/lifeStyle/2.png';
+import lifeStyleList3 from '@/assets/images/home/lists/lifeStyle/3.png';
+
+import foodAndDrinkList1 from '@/assets/images/home/lists/foodAndDrink/1.png';
+import foodAndDrinkList2 from '@/assets/images/home/lists/foodAndDrink/2.png';
+import foodAndDrinkList3 from '@/assets/images/home/lists/foodAndDrink/3.png';
+
+import travelList1 from '@/assets/images/home/lists/travel/1.png';
+import travelList2 from '@/assets/images/home/lists/travel/2.png';
+import travelList3 from '@/assets/images/home/lists/travel/3.png';
+
+import cultureList1 from '@/assets/images/home/lists/culture/1.png';
+import cultureList2 from '@/assets/images/home/lists/culture/2.png';
+import cultureList3 from '@/assets/images/home/lists/culture/3.png';
+
+import entertainmentList1 from '@/assets/images/home/lists/entertainment/1.png';
+import entertainmentList2 from '@/assets/images/home/lists/entertainment/2.png';
+import entertainmentList3 from '@/assets/images/home/lists/entertainment/3.jpg';
+
+import techAndDigitalList1 from '@/assets/images/home/lists/techAndDigital/1.png';
+import techAndDigitalList2 from '@/assets/images/home/lists/techAndDigital/2.png';
+import techAndDigitalList3 from '@/assets/images/home/lists/techAndDigital/3.png';
+
+import personalGrowthList1 from '@/assets/images/home/lists/personalGrowth/1.png';
+import personalGrowthList2 from '@/assets/images/home/lists/personalGrowth/2.png';
+import personalGrowthList3 from '@/assets/images/home/lists/personalGrowth/3.png';
+
+import healthAndFitnessList1 from '@/assets/images/home/lists/healthAndFitness/1.jpg';
+import healthAndFitnessList2 from '@/assets/images/home/lists/healthAndFitness/2.jpg';
+import healthAndFitnessList3 from '@/assets/images/home/lists/healthAndFitness/3.jpg';
+
+import otherList1 from '@/assets/images/home/lists/other/1.png';
+import otherList2 from '@/assets/images/home/lists/other/2.png';
+import otherList3 from '@/assets/images/home/lists/other/3.png';
+
+// feature
+import leftTop from '@/assets/images/home/feature/left-top.png';
+import leftBottom from '@/assets/images/home/feature/left-bottom.png';
+import leftMid from '@/assets/images/home/feature/left-mid.png';
+import midTop from '@/assets/images/home/feature/mid-top.png';
+import rightTop from '@/assets/images/home/feature/right-top.png';
+import rightMid from '@/assets/images/home/feature/right-mid.png';
+import rightBottom from '@/assets/images/home/feature/right-bottom.png';
+
 export const IMAGES = {
   avatar: {
-    lifeStyle: '/src/assets/images/home/avatar/life-style.png',
-    foodAndDrink: '/src/assets/images/home/avatar/food-drink.png',
-    travel: '/src/assets/images/home/avatar/travel.png',
-    culture: '/src/assets/images/home/avatar/culture.png',
-    entertainment: '/src/assets/images/home/avatar/entertainment.png',
-    personalGrowth: '/src/assets/images/home/avatar/personal-growth.png',
-    techAndDigital: '/src/assets/images/home/avatar/tech-digital.png',
-    healthAndFitness: '/src/assets/images/home/avatar/health-fitness.png',
-    other: '/src/assets/images/home/avatar/other.png',
+    lifeStyle: lifeStyleAvatar,
+    foodAndDrink: foodAndDrinkAvatar,
+    travel: travelAvatar,
+    culture: cultureAvatar,
+    entertainment: entertainmentAvatar,
+    personalGrowth: personalGrowthAvatar,
+    techAndDigital: techAndDigitalAvatar,
+    healthAndFitness: healthAndFitnessAvatar,
+    other: otherAvatar,
   },
   list: {
     lifeStyle: {
-      list1: '/src/assets/images/home/lists/lifeStyle/1.png',
-      list2: '/src/assets/images/home/lists/lifeStyle/2.png',
-      list3: '/src/assets/images/home/lists/lifeStyle/3.png',
+      list1: lifeStyleList1,
+      list2: lifeStyleList2,
+      list3: lifeStyleList3,
     },
     foodAndDrink: {
-      list1: '/src/assets/images/home/lists/foodAndDrink/1.png',
-      list2: '/src/assets/images/home/lists/foodAndDrink/2.png',
-      list3: '/src/assets/images/home/lists/foodAndDrink/3.png',
+      list1: foodAndDrinkList1,
+      list2: foodAndDrinkList2,
+      list3: foodAndDrinkList3,
     },
     culture: {
-      list1: '/src/assets/images/home/lists/culture/1.png',
-      list2: '/src/assets/images/home/lists/culture/2.png',
-      list3: '/src/assets/images/home/lists/culture/3.png',
+      list1: cultureList1,
+      list2: cultureList2,
+      list3: cultureList3,
     },
     travel: {
-      list1: '/src/assets/images/home/lists/travel/1.png',
-      list2: '/src/assets/images/home/lists/travel/2.png',
-      list3: '/src/assets/images/home/lists/travel/3.png',
+      list1: travelList1,
+      list2: travelList2,
+      list3: travelList3,
     },
     entertainment: {
-      list1: '/src/assets/images/home/lists/entertainment/1.png',
-      list2: '/src/assets/images/home/lists/entertainment/2.png',
-      list3: '/src/assets/images/home/lists/entertainment/3.jpg',
+      list1: entertainmentList1,
+      list2: entertainmentList2,
+      list3: entertainmentList3,
     },
     techAndDigital: {
-      list1: '/src/assets/images/home/lists/techAndDigital/1.png',
-      list2: '/src/assets/images/home/lists/techAndDigital/2.png',
-      list3: '/src/assets/images/home/lists/techAndDigital/3.png',
+      list1: techAndDigitalList1,
+      list2: techAndDigitalList2,
+      list3: techAndDigitalList3,
     },
     personalGrowth: {
-      list1: '/src/assets/images/home/lists/personalGrowth/1.png',
-      list2: '/src/assets/images/home/lists/personalGrowth/2.png',
-      list3: '/src/assets/images/home/lists/personalGrowth/3.png',
+      list1: personalGrowthList1,
+      list2: personalGrowthList2,
+      list3: personalGrowthList3,
     },
     healthAndFitness: {
-      list1: '/src/assets/images/home/lists/healthAndFitness/1.jpg',
-      list2: '/src/assets/images/home/lists/healthAndFitness/2.jpg',
-      list3: '/src/assets/images/home/lists/healthAndFitness/3.jpg',
+      list1: healthAndFitnessList1,
+      list2: healthAndFitnessList2,
+      list3: healthAndFitnessList3,
     },
     other: {
-      list1: '/src/assets/images/home/lists/other/1.png',
-      list2: '/src/assets/images/home/lists/other/2.png',
-      list3: '/src/assets/images/home/lists/other/3.png',
+      list1: otherList1,
+      list2: otherList2,
+      list3: otherList3,
     },
   },
   feature: {
-    leftTop: '/src/assets/images/home/feature/left-top.png',
-    leftBottom: '/src/assets/images/home/feature/left-bottom.png',
-    leftMid: '/src/assets/images/home/feature/left-mid.png',
-    midTop: '/src/assets/images/home/feature/mid-top.png',
-    rightTop: '/src/assets/images/home/feature/right-top.png',
-    rightMid: '/src/assets/images/home/feature/right-mid.png',
-    rightBottom: '/src/assets/images/home/feature/right-bottom.png',
+    leftTop: leftTop,
+    leftBottom: leftBottom,
+    leftMid: leftMid,
+    midTop: midTop,
+    rightTop: rightTop,
+    rightMid: rightMid,
+    rightBottom: rightBottom,
   },
 };
