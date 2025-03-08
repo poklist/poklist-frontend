d6754fcb65856ea6542ded0a6d17e97396731ee0
jamespig <james7766646@gmail.com>
2025-03-08 10:51:32 +0800

    feat: Fill connect

--- src/pages/Layout/Components/BottomNav.tsx
+++ src/pages/Layout/Components/BottomNav.tsx
@@ -2,15 +2,27 @@
 const BottomNav = () => {
   return (
     <div className="text-tl hidden flex-row justify-center space-x-2 self-end py-2 font-bold text-black-text-01 sm:flex">
-      <a href="/about" target="_blank">
+      <a
+        href="https://opaque-creek-8e5.notion.site/Poklist-About-Poklist-1a8a4cd4b98b80868769d3f690d094ab"
+        target="_blank"
+        rel="noreferrer"
+      >
         About
       </a>
       <span>|</span>
-      <a href="/terms" target="_blank">
+      <a
+        href="https://opaque-creek-8e5.notion.site/Terms-of-Service-1a9a4cd4b98b80ebb5cee016b5c88089"
+        target="_blank"
+        rel="noreferrer"
+      >
         Terms
       </a>
       <span>|</span>
-      <a href="/privacy" target="_blank">
+      <a
+        href="https://opaque-creek-8e5.notion.site/Privacy-Policy-1a9a4cd4b98b8098a2c4fa29619981dc"
+        target="_blank"
+        rel="noreferrer"
+      >
         Privacy
       </a>
     </div>
